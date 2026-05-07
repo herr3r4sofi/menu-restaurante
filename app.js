3f8ffbbbbd5474612229698dfb36e6dabc4dfdf2
Nahum <nahupclolsuperman@gmail.com>
2026-05-07 13:46:41 -0600

    Inicializar conexion con Firebase

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 let db = null;
 let filterCat = 'all';
+
+function initFirebase(){
+    try{
+        initFirebase.initializeApp(firebaseConfig);
+        db = firebase.database();
+
+        db.ref('.info/connected').on('value', snap => {
+            setDbStatus(snap.val() === true ? 'connected' : 'disconnected');
+        });
+
+        startListen();
+    } catch (e) {
+        showToast('Eror al conectar con Firebase: '+ e.message, 'error');
+        setDbStatus('error');
+    }
+}
